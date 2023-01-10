31d825d9dc0c55046db13b5356ec0366d36f2dc9
Tony Tran <92814271+NightToki@users.noreply.github.com>
2023-01-10 15:18:47 -0800

    Add files via upload

---
+++ MegaWar1.2/LostAndFound.h
@@ -0,0 +1,12 @@
+#ifndef LOSTANDFOUND_H
+#define LOSTANDFOUND_H
+#include "CardPile.h"
+#include "Player.h"
+
+class LostAndFound : public CardPile {
+public:
+	void display();
+	Card deal();
+};
+#endif
+
---
+++ MegaWar1.2/WarPile.cpp
@@ -0,0 +1,14 @@
+#include "WarPile.h"
+
+void WarPile::display() {
+	for (Card c : pile) {
+		c.showCard();
+		cout << " ";
+	}
+}
+
+Card WarPile::deal() {
+	Card top = pile.back();
+	remove();
+	return top;
+}
